aa453acfe828e8f2fa6146ec8d9f8c3bd28ccc0e
NuaCho <nvf-2002@mail.ru>
2024-03-11 20:29:57 +0400

    Написал программу

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+class Program{
+    static void Main(){
+        string[] inputArray = {"ghsg", "fff", "hrsdijghd", "af", "jdfhbbejn"};
+        string[] newArray = new string[inputArray.Length];
+        int index = 0;
+        foreach (string str in inputArray){
+            if (str.Length <= 3){
+                newArray[index] = str;
+                index++;
+            }
+        }
+    for (int i = 0; i < index; i++) {
+        Console.WriteLine(newArray[i]);
+    }
+    }
+}
